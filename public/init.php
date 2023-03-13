117c5404ae10b276db52ae597e22822a614f8a15
marlowe cheng <114950923+marlowecheng@users.noreply.github.com>
2023-03-13 00:07:51 -0700

    https not http

--- public/init.php
+++ public/init.php
@@ -1,6 +1,6 @@
 <?php 
     
-    define('WWW_ROOT', 'http://marlowecheng.com');
+    define('WWW_ROOT', 'https://marlowecheng.com');
     // define('WWW_ROOT', 'http://localhost');
     define('PROJECT_ROOT', dirname(__DIR__, 1));
 
