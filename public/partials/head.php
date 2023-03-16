c23f28d18ec7f1a8404d2c17303cc2f699f90f6a
marlowe cheng <114950923+marlowecheng@users.noreply.github.com>
2023-03-16 16:49:07 -0700

    deleted unused links in head

--- public/partials/head.php
+++ public/partials/head.php
@@ -3,8 +3,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Marlowe Cheng</title>
-    <link rel="stylesheet" href="<?php //echo get_public_url('css/reset.css'); ?>">
-    <link rel="stylesheet" href="<?php //echo get_public_url('css/lightbox.css'); ?>">
     <link rel="stylesheet" href="<?php echo get_public_url('css/main.min.css?v=1.1'); ?>">
     <link rel="icon" href="images/marlowe-cheng-favicon.png" type="image/png">
     <script src="https://kit.fontawesome.com/4082a6b417.js" crossorigin="anonymous"></script>
