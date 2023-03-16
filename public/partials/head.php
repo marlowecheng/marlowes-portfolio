6c7db2bf8d4092ff24e8bd0729d64f5571600478
marlowe cheng <114950923+marlowecheng@users.noreply.github.com>
2023-03-16 16:24:22 -0700

    Updates deploy script

--- public/partials/head.php
+++ public/partials/head.php
@@ -5,7 +5,7 @@
     <title>Marlowe Cheng</title>
     <link rel="stylesheet" href="<?php //echo get_public_url('css/reset.css'); ?>">
     <link rel="stylesheet" href="<?php //echo get_public_url('css/lightbox.css'); ?>">
-    <link rel="stylesheet" href="<?php echo get_public_url('css/main.min.css'); ?>">
+    <link rel="stylesheet" href="<?php echo get_public_url('css/main.min.css?v=1.1'); ?>">
     <link rel="icon" href="images/marlowe-cheng-favicon.png" type="image/png">
     <script src="https://kit.fontawesome.com/4082a6b417.js" crossorigin="anonymous"></script>
 </head>
