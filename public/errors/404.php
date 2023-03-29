52b28b10499751eb15ff2e82bbe1566f5fa23625
marlowe cheng <114950923+marlowecheng@users.noreply.github.com>
2023-03-28 17:31:31 -0700

    404 changes

--- public/errors/404.php
+++ public/errors/404.php
@@ -11,12 +11,11 @@
                 <div class="grid-col">
                     <div class="column">
                         <h1>This page does not exist.</h1>
-                        <a href="<?php echo get_public_url('index.php'); ?>" title="Go back to Home" class="btn">Let's go back home.</a>
+                        <a href="<?php echo get_public_url('index.php'); ?>" title="Go back to Home" class="btn">Let's go back home</a>
                     </div>
                 </div>
             </div>
         </section>
     </main>
-    <?php // require(get_path('public/partials/footer.php')); ?>
 </body>
 </html>
