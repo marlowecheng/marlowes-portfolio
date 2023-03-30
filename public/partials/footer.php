2c001dd73688ab6af9ea757421e657ffa87569b1
marlowe cheng <114950923+marlowecheng@users.noreply.github.com>
2023-03-30 15:33:45 -0700

    fixed footer date to auto update year

--- public/partials/footer.php
+++ public/partials/footer.php
@@ -5,7 +5,7 @@
                 <div class="footer-logo">
                     <a href="<?php echo get_public_url('index.php'); ?>" title="Go back to home"><img src="<?php echo get_public_url('images/marlowe-cheng-logo-alt.svg'); ?>" alt="Marlowe's logo"></a>
                 </div>
-                <p>&copy; Marlowe Cheng 2022</p>
+                <p>&copy; Marlowe Cheng <?php echo date("Y"); ?> &mdash; Fueled by 🥟 and therapy</p>
                 <div class="socials">
                     <a href="mailto:marlowe.cheng@gmail.com" title="Email Marlowe" target="_blank"><span><i class="fa-solid fa-envelope"></i></span></a>
                     <a href="https://www.linkedin.com/in/marlowe-cheng/" title="Go to Marlowe's LinkedIn Profile" target="_blank"><span><i class="fa-brands fa-linkedin-in"></i></span></a>
