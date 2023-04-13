5292e29d38815ab7232a0ef720e28f6a949e9ade
marlowe cheng <114950923+marlowecheng@users.noreply.github.com>
2023-04-12 23:38:18 -0700

    added github to contact and footer

--- public/partials/footer.php
+++ public/partials/footer.php
@@ -1,14 +1,15 @@
 <footer>
     <div class="container">
         <div class="grid-col">
-            <div class="column">
+            <div class="column footer-content">
                 <div class="footer-logo">
                     <a href="<?php echo get_public_url('index.php'); ?>" title="Go back to home"><img src="<?php echo get_public_url('images/marlowe-cheng-logo-alt.svg'); ?>" alt="Marlowe's logo"></a>
                 </div>
                 <p>&copy; Marlowe Cheng <?php echo date("Y"); ?> &mdash; Fueled by 🥟 and therapy</p>
                 <div class="socials">
-                    <a href="mailto:marlowe.cheng@gmail.com" title="Email Marlowe" target="_blank"><span><i class="fa-solid fa-envelope"></i></span></a>
+                    <a href="mailto:contact@marlowecheng.com" title="Email Marlowe" target="_blank"><span><i class="fa-solid fa-envelope"></i></span></a>
                     <a href="https://www.linkedin.com/in/marlowe-cheng/" title="Go to Marlowe's LinkedIn Profile" target="_blank"><span><i class="fa-brands fa-linkedin-in"></i></span></a>
+                    <a href="https://github.com/marlowecheng" target="_blank" title="View Marlowe's GitHub"><span><i class="fa-brands fa-github"></i></span></a>
                     <a href="https://www.discordapp.com/users/138401488274718721" title="Add Marlowe on Discord (Only works on Discord Web Version)" target="_blank"><span><i class="fa-brands fa-discord"></i></span></a>
                 </div>
             </div>
